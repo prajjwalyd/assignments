aca972aa9f9bd12608adf5efecd97685daefaaa7
Prajjwal Yadav <prajjwalyd@gmail.com>
2023-12-10 01:39:41 +0530

    setting up

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,49 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryMap = {};
+
+  transactions.forEach(transaction => {
+    const { category, price } = transaction;
+
+    if (categoryMap[category]) {
+      categoryMap[category].totalSpent += price;
+    } else {
+      categoryMap[category] = { category, totalSpent: price };
+    }
+  });
+
+  const result = Object.values(categoryMap);
+  return result;
 }
+
+// Example usage:
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656176800000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656276800000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'Shirt',
+  },
+];
+
+const result = calculateTotalSpentByCategory(transactions);
+console.log(result);
+
 
 module.exports = calculateTotalSpentByCategory;
